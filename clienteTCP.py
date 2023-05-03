d75e5f765eecfab8cad7faadbce169c85d6b5463
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2023-05-03 17:34:40 -0300

    Tradutor básico

---
+++ clienteTCP.py
@@ -0,0 +1,33 @@
+import socket
+
+HOST = 'localhost' # maquina onde esta o par passivo
+PORTA = 5000        # porta que o par passivo esta escutando
+
+# cria socket
+sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM 
+
+# conecta-se com o par passivo
+sock.connect( ( HOST, PORTA ) ) 
+
+print( f'\nT R A D U T O R - D E - R E D E S\n\n'+
+      f'Digite uma das palavras a seguir para descobrir sua tradução em inglês:\n'+
+      f'- rede - enlace - link - roteador -\n'+
+      f'- comutador - cliente - servidor - hospedeiro -\n' )
+while True:
+    # define a string para envio da mensagem ou codigo de parada
+    aux = input( 'Digite a palavra: ' )
+
+    # codigo de parada
+    if aux == 'stop': break
+    
+    # envia uma mensagem para o par conectado
+    sock.send(bytes(aux, 'utf-8'))
+    
+    #espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
+    msg = sock.recv(1024) # argumento indica a qtde maxima de bytes da mensagem
+
+    # imprime a mensagem recebida
+    print(str(msg,  encoding='utf-8'))
+
+# encerra a conexao
+sock.close() 
