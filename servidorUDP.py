dc03c2c20a70f99c6ecb1e870d4d678e1427eda9
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2023-05-03 18:13:16 -0300

    Tradutor básico - UDP

---
+++ servidorUDP.py
@@ -0,0 +1,22 @@
+import socket
+
+UDP_IP = "127.0.0.1" # endereço IP local
+UDP_PORT = 5005      # porta que o servidor vai escutar
+
+# dicionário para traduzir as palavras relacionadas a redes, do português para o inglês
+dicio_redes = {'rede':b'network', 'enlace':b'link', 'roteador':b'router', 
+	       'comutador':b'switch', 'cliente':b'client', 'servidor':b'server',
+	       'hospedeiro':b'host'}
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+print("Servidor de echo UDP iniciado em {}:{}".format(UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # espera por uma mensagem
+    if ( dicio_redes.get( str( data, encoding='utf-8') ) is not None ):
+        data_traduzida = dicio_redes[ str( data, encoding='utf-8' )]
+    print("Mensagem recebida de {}: {}".format(addr, data))
+    sock.sendto(data_traduzida, addr) # envia a mensagem de volta para o endereço que enviou
