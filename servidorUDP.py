8df19a070071a17b91b09b96ca416f1755259a9f
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2023-05-07 23:53:50 -0300

    Tradutor com as 10 palavras

--- servidorUDP.py
+++ servidorUDP.py
@@ -6,7 +6,8 @@ UDP_PORT = 5005      # porta que o servidor vai escutar
 # dicionário para traduzir as palavras relacionadas a redes, do português para o inglês
 dicio_redes = {'rede':b'network', 'enlace':b'link', 'roteador':b'router', 
 	       'comutador':b'switch', 'cliente':b'client', 'servidor':b'server',
-	       'hospedeiro':b'host'}
+	       'hospedeiro':b'host', 'navegador':b'browser', 'endereço IP':b'IP address',
+	       'fim-a-fim':b'pier-to-pier'}
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -18,5 +19,7 @@ while True:
     data, addr = sock.recvfrom(1024) # espera por uma mensagem
     if ( dicio_redes.get( str( data, encoding='utf-8') ) is not None ):
         data_traduzida = dicio_redes[ str( data, encoding='utf-8' )]
-    print("Mensagem recebida de {}: {}".format(addr, data))
-    sock.sendto(data_traduzida, addr) # envia a mensagem de volta para o endereço que enviou
+    print("Mensagem de {}: {}".format(addr, data))
+
+		# envia a mensagem de volta para o endereço que enviou
+    sock.sendto(data_traduzida, addr)
