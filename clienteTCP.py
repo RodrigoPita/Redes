ac6b781a3ec707a08ca7c580041ed69e66b0d234
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2023-05-07 23:54:59 -0300

    Tradutor com as 10 palavras

--- clienteTCP.py
+++ clienteTCP.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = 'localhost' # maquina onde esta o par passivo
-PORTA = 5000        # porta que o par passivo esta escutando
+PORTA = 5005        # porta que o par passivo esta escutando
 
 # cria socket
 sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM 
@@ -12,7 +12,8 @@ sock.connect( ( HOST, PORTA ) )
 print( f'\nT R A D U T O R - D E - R E D E S\n\n'+
       f'Digite uma das palavras a seguir para descobrir sua tradução em inglês:\n'+
       f'- rede - enlace - link - roteador -\n'+
-      f'- comutador - cliente - servidor - hospedeiro -\n' )
+      f'- comutador - cliente - servidor - hospedeiro -\n'+
+      f'- navegador - endereço IP - fim-a-fim -\n'  )
 while True:
     # define a string para envio da mensagem ou codigo de parada
     aux = input( 'Digite a palavra: ' )
@@ -30,4 +31,4 @@ while True:
     print(str(msg,  encoding='utf-8'))
 
 # encerra a conexao
-sock.close() 
+sock.close()
